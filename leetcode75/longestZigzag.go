045aad0c7f61692dbc5760c8c3a4c7b6d0fccdae
shuvoedward <38678545+shuvoedward@users.noreply.github.com>
2026-04-10 02:33:10 -0400

    longest zigzag in bst

--- leetcode75/longestZigzag.go
+++ leetcode75/longestZigzag.go
@@ -1,5 +1,30 @@
 package leetcode75
 
 func longestZigZag(root *TreeNode) int {
-	return 0
+	maxLength := 0
+
+	var dfs func(root *TreeNode, goLeft bool, length int)
+
+	dfs = func(root *TreeNode, goLeft bool, length int) {
+		if root == nil {
+			return
+		}
+
+		if length > maxLength {
+			maxLength = length
+		}
+
+		if goLeft {
+			dfs(root.Left, false, length+1)
+			dfs(root.Right, true, 1)
+		} else {
+			dfs(root.Right, true, length+1)
+			dfs(root.Left, false, 1)
+		}
+	}
+
+	dfs(root, true, 0)
+	dfs(root, false, 0)
+
+	return maxLength
 }
